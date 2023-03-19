89d900ff23dc6bc2588a12622c4c7f0f4bc13097
Cristie Ferreira <cristie_lopes_ferreira@hotmail.com>
2023-03-19 15:34:25 -0300

    testando a criação do método post

--- src/main/kotlin/com/example/apilogin/models/User.kt
+++ src/main/kotlin/com/example/apilogin/models/User.kt
@@ -10,16 +10,21 @@ import jakarta.persistence.Table
 
 @Entity()
 @Table(name = "users_tb")
-class User(
+data class User(
+
    @Id
    @GeneratedValue(strategy = GenerationType.SEQUENCE)
-   val id: Int,
+   val id: Long,
+
    @Column(name = "name", nullable = false)
    val name: String,
+
    @Column(name = "email", nullable = false)
    val email: String,
+
    @Column(name = "phone", nullable = false)
    val phone: String,
+
    @Column(name = "password", nullable = false)
    val password: String,
 )
--- src/main/kotlin/com/example/apilogin/service/UserService.kt
+++ src/main/kotlin/com/example/apilogin/service/UserService.kt
@@ -3,6 +3,7 @@ package com.example.apilogin.service
 import com.example.apilogin.models.User
 import com.example.apilogin.repository.UserRepository
 import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.RequestBody
 import java.util.Optional
 
 @Service
@@ -10,5 +11,9 @@ class UserService(val userRepository: UserRepository) {
 
     fun findUserById(id: Int): Optional<User> {
         return userRepository.findById(id)
+    }
+
+    fun postFunction(@RequestBody() userEdit: User): User{
+        return userEdit;
     }
 }
